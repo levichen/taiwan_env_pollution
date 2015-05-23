31e2bc309ece83be2b94499d7064d3674879af94
Yi Z <mileskies@gmail.com>
2015-05-23 14:58:05 +0800

    add search content

--- dev/entry.js
+++ dev/entry.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var MainContent = require('./components/main_content');
 var NavBar = require('./components/navbar');
+var SearchContent = require('./components/SearchContent');
 
 // <NavBar />
 React.render(
@@ -8,6 +9,7 @@ React.render(
    		<div className="mainContainer-bg" />
         <NavBar />
    	    <MainContent />
+   	    <SearchContent />
 	</div> ,
    	document.getElementsByTagName('body')[0]
 );
