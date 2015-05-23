4122847d318181f36452e49e6a3dc02b42e20b2e
Yi Z <mileskies@gmail.com>
2015-05-23 10:05:26 +0800

    add draw color

--- dev/stores/NavBarStore.js
+++ dev/stores/NavBarStore.js
@@ -13,7 +13,8 @@ var NavBarStore = Reflux.createStore({
 				"sub": [
 					{
 						"sub_id": "factory",
-						"sub_title": "工廠排放"
+						"sub_title": "工廠排放",
+						"sub_color": "#2B2C88"
 					},
 					// {
 					// 	"sub_id": 1,
@@ -27,19 +28,23 @@ var NavBarStore = Reflux.createStore({
 				"sub": [
 					{
 						"sub_id": "incinerators",
-						"sub_title": "焚化爐"
+						"sub_title": "焚化爐",
+						"sub_color": "#88342B"
 					},
 					{
 						"sub_id": "garbage",
-						"sub_title": "垃圾場"
+						"sub_title": "垃圾場",
+						"sub_color": "#5864E7"
 					},
 					{
 						"sub_id": "airstation",
-						"sub_title": "空氣污染站"
+						"sub_title": "空氣污染站",
+						"sub_color": "#2D7679"
 					},
 					{
 						"sub_id": "petition",
-						"sub_title": "民眾陳情地點"
+						"sub_title": "民眾陳情地點",
+						"sub_color": "#66304E"
 					}
 				]
 			}
